38e1a00f895b7da07e8aa9f257317f3daf5b04ad
miueon <cruxbell@gmail.com>
2024-01-13 22:54:25 +0800

    base

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./modules/backend/src/main/resources/index.html",
+    "./modules/frontend/src/**/*.scala",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
