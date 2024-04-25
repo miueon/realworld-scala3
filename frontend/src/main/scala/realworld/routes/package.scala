9950702265b8b5e3130da5b82a9bc49e418da54f
miueon <cruxbell@gmail.com>
2024-04-25 19:24:14 +0800

    add new route

--- frontend/src/main/scala/realworld/routes/package.scala
+++ frontend/src/main/scala/realworld/routes/package.scala
@@ -7,6 +7,7 @@ import realworld.routes.Page.ProfilePage
 import realworld.spec.Username
 import realworld.routes.Page.ArticleDetailPage
 import realworld.spec.Slug
+import realworld.routes.Page.EditArticlePage
 package object routes:
   def fragmentStatic[Page](staticPage: Page, pattern: PathSegment[Unit, DummyError]) =
     Route.static(staticPage, pattern, Route.fragmentBasePath)
@@ -25,12 +26,21 @@ package object routes:
     Route.fragmentBasePath
   )
 
+  val editArticlePageRoute = Route(
+    encode = (stp: EditArticlePage) => stp.slug.value,
+    decode = (arg: String) => EditArticlePage(Slug(arg)),
+    pattern = root / "editor" / segment[String] / endOfSegments,
+    Route.fragmentBasePath
+  )
+
   val routes = List(
     fragmentStatic(Page.Home, root / endOfSegments),
     fragmentStatic(Page.Login, root / "login"),
     fragmentStatic(Page.Register, root / "register"),
     fragmentStatic(Page.Setting, root / "settings"),
+    fragmentStatic(Page.NewArticle, root / "editor"),
     articleDetailPageRoute,
-    profileRoute
+    profileRoute,
+    editArticlePageRoute
   )
 end routes
